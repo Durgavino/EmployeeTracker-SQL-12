9a1b7120a241900c479ac99593981433c65238f0
Durgavino <mailtodurga93@gmail.com>
2023-01-31 15:29:42 -0400

    updated the view all role

--- index.js
+++ index.js
@@ -3,19 +3,18 @@ const mysql = require('mysql2');
 const consoleTable = require('console.table');
 const path = require('path');
 
-// Connect to database
+//Connect to database
 const db = mysql.createConnection(
   {
     host: 'localhost',
-    // MySQL username,
+   // MySQL username,
     user: 'root',
-    // MySQL password
+  // MySQL password
     password: 'bootcamp',
     database: 'EmployeeTracker_db'
   },
   console.log(`Connected to the EmployeeTracker_db database.`)
 );
-
 
 const options = [
   {
@@ -36,47 +35,41 @@ const options = [
 ];
 
 
-function init() {
+
+function init(){
   inquirer.prompt(options)
-    .then(data => {
-      console.log(data);
-if("View all Departments"==="View all Departments"){
-  return viewallDepartment();
+  .then(data =>{
+    console.log(data);
+    if("View all Departments"==="View all Departments"){
+      return viewallDepartment();
+    }
+else if("View all Roles"==="View all Roles"){
+  return viewallRole();
 }
-    })
+
+
+
+  })
 }
 
 init();
 
-function viewallDepartment(){
+
+
+function viewallDepartment() {
   // Query database
-db.query('SELECT * FROM department', function (err, results) {
-  console.log(results);
-});
+  db.query('SELECT * FROM department', function (err, results) {
+    console.log("------------------------");
+   console.log(results);
+   
+   console.log("------------------------");
+  });
 }
 
 
-
-
-
-
-
-
-
-// db.query('SELECT * FROM role', function (err, results) {
-//   console.log(results);
-// });
-
-// db.query('SELECT * FROM employee', function (err, results) {
-//   console.log(results);
-// });
-
-
-
-// app.use((req, res) => {
-//   res.status(404).end();
-// });
-
-// app.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`);
-// });
+function viewallRole() {
+  db.query('select role.id,role.title,role.salary,department.dept_name from role join department on role.department_id=department.department_id; ', function (err, results) {
+    if (err) throw err;
+    console.log(results);
+  });
+}
