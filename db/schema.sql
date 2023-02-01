82a5693ab8fc5e76f95a30a22e43d88bfb372ca3
Durgavino <mailtodurga93@gmail.com>
2023-02-01 15:58:11 -0400

    updated sql code

--- db/schema.sql
+++ db/schema.sql
@@ -1,27 +1,22 @@
-DROP DATABASE IF EXISTS EmployeeTracker_db;
-CREATE DATABASE EmployeeTracker_db;
-
-USE EmployeeTracker_db;
-
+DROP DATABASE IF EXISTS employeetracker_db;
+CREATE DATABASE employeetracker_db;
+USE employeetracker_db;
 CREATE TABLE department (
   department_id INT PRIMARY KEY,
   dept_name VARCHAR(30)
 );
-
-
 CREATE TABLE role(
     id INT PRIMARY KEY,
     title VARCHAR(30),
     salary decimal,
-    department_id INT
+    department_id INT,
+    foreign key (department_id) references department (department_id)
 );
-
 CREATE TABLE employee(
 id int primary key auto_increment,
       first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT not null,
     manager_id INT
-    
 );
 
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,4 @@
+
 INSERT INTO department (department_id,dept_name)
 VALUES (1, "Sales"),
        (2, "Engineer"),
@@ -5,11 +6,7 @@ VALUES (1, "Sales"),
        (4, "HR"),
        (5,"Training");
        
-
-       
-       select * from department;
-       
-       INSERT INTO role (id,title,salary,department_id)
+         INSERT INTO role (id,title,salary,department_id)
 VALUES (1, "Sales Lead",5000,1),
        (2, "Sales Person",3000,1),
        (3, "Account Manager",6000,3),
@@ -18,30 +15,32 @@ VALUES (1, "Sales Lead",5000,1),
        (6,"Trainer",4000,5),
        (7,"Senior Engineer",6000,2),
        (8,"Junior Engineer",3000,2);
-
- select * from role;
        
        
-        INSERT INTO employee (first_name,last_name,role_id,manager_id)
-VALUES ("Vinoth","Venkatesan",1,1),
-       ("Jon","Gaston",2,1),
-      ("Evelyn","James",3,2),
-       ("Eva","Robert",4,2),
-       ("Maya","Dev",5,2),
-       ("Dave","Thomas",6,2),
-       ("Jermy","paul",7,1),
-       ("Stefan","Matt",8,1);
-
+         INSERT INTO employee (id,first_name,last_name,role_id,manager_id)
+VALUES (1,"Vinoth","Venkatesan",1,1),
+       (2,"Jon","Gaston",2,1),
+      (3,"Evelyn","James",3,2),
+       (4,"Eva","Robert",4,2),
+       (5,"Maya","Dev",5,2),
+       (6,"Dave","Thomas",6,2),
+       (7,"Jermy","paul",7,1),
+       (8,"Stefan","Matt",8,1);
+       
+       
+       select * from department;
+       select * from role;
 select * from employee;
 
-
+show databases;
+ 
  alter table role add foreign key (department_id) references department(department_id);
  
-select role.id,role.title,role.salary,department.dept_name
+ select role.id,role.title,role.salary,department.dept_name
  from role
  join department
  on role.department_id=department.department_id;
-
-
-
- select
+ 
+ 
+ 
+ show databases;
